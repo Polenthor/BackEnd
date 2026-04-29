1272274f5ee0af285d26c0110131f252090a3a3f
Polenthor <shiduaboobacker@gmail.com>
2026-04-29 20:23:07 +0400

    r

--- connection.js
+++ connection.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config(); // Load variables from a .env file
+//const mongoose = require('mongoose');
+//require('dotenv').config(); // Load variables from a .env file
 
 // Use an environment variable for the URI
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://aboobacker:aboobacker@cluster0.zvwxqje.mongodb.net/?appName=Cluster0';
+//const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://aboobacker:aboobacker@cluster0.zvwxqje.mongodb.net/?appName=Cluster0';
 
-mongoose.connect(mongoURI)
-  .then(() => console.log('Connected Successfully to MongoDB'))
-  .catch((err) => console.log('Connection Error:', err));
+//mongoose.connect(mongoURI)
+ // .then(() => console.log('Connected Successfully to MongoDB'))
+// .catch((err) => console.log('Connection Error:', err));
+
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch((err) => console.log('❌ DB error:', err));
